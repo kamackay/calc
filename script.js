0b36473b6d9a572f59be878292b14dbd27d1cc56
Keith MacKay <kmackay@corp.ipipenet.com>
2016-06-23 11:11:29 -0400

    update

--- script.js
+++ script.js
@@ -4,15 +4,53 @@ function showInOutput(str, newLn = true, tab = 0) {
     for (var i = 0; i < tab; i++) out.append('    ');
     out.append(str);
 }
+var win = $(window);
 
 
+var f = function () {
+    var size = win.height() / 10;
+    var mobile = isMobileDevice();
+    var calc = $('body').find('.calculator');
+    var numbers = calc.find('.numbers');
+    var buttons = calc.find('.btn');
+    if (mobile || win.height() > win.width()) calc.css('width', '100%');
+    else calc.css('width', '60%');
+    var w = (numbers.width() / 4) - 1;
+    $.each(buttons, function (n, o) {
+        $(this).css('font-size', size / 2).css('width', w);
+    });
+    calc.find('.console').css('height', size * 2).css('width', w * 4);
+    $('#bottomButtons').css('height', size * 3);
+};
 
 ($(document).ready(function () {
     $('#output').html("Output:<br>");
+    f();
+    $(window).resize(f);
+    con = $('#numberIn');
 }));
 
-function calculate() {
+var con;
 
+function calcButton(btn) {
+    if (btn === 'clear') {
+        con.html('');
+    } else if (btn === 'plus') {
+
+    } else if (btn === 'minus') {
+
+    } else if (btn === 'equal') {
+
+    } else if (btn === 'period') {
+
+    } else if (btn === 'multiply') {
+
+    } else {
+        con.append(btn);
+    }
+}
+
+function calculate() {
     getAllPrimes(parseInt($('#numberInput').val()));
 }
 
@@ -23,15 +61,10 @@ function getAllPrimes(n = 50) {
         n = n / 2;
     }
     for (var i = 3; i <= Math.sqrt(n); i = i + 2) {
-        // While i divides n, print i and divide n
         while (n % i == 0) {
             showInOutput(i.toString());
             n = n / i;
         }
     }
-
-    // This condition is to handle the case whien n is a prime number
-    // greater than 2
-    if (n > 2)
-        showInOutput(n.toString());
+    if (n > 2) showInOutput(n.toString());
 }
